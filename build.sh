704ed3f2cf2aec0201637360983feb7cd21a3f13
Mark Probst <mark.probst@gmail.com>
2016-04-21 15:27:44 -0700

    Don't use docker tag -f flag.

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ env GOOS=linux GOARCH=amd64 go build -o reloadcache *.go
 docker build -t reloadcache .
 rm reloadcache
 
-docker tag -f reloadcache 633007691302.dkr.ecr.us-east-1.amazonaws.com/reloadcache:latest
+docker tag reloadcache 633007691302.dkr.ecr.us-east-1.amazonaws.com/reloadcache:latest
 docker push 633007691302.dkr.ecr.us-east-1.amazonaws.com/reloadcache:latest
